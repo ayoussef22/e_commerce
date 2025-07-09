6b1505a5b9cb22997560b9929711f6e50fe969f8
abdallah adel <abdallah.aadel123@gmail.com>
2025-07-10 00:19:29 +0300

    Resolve the splash screen problem

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -3,14 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce_c11_online/core/resources/assets_manager.dart';
 import '../../core/routes_manager/routes.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
     Timer(Duration(seconds: 3), () {
-      Navigator.pushNamed(context, Routes.signInRoute);
+      Navigator.pushReplacementNamed(context, Routes.signInRoute);
     });
+  }
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: Image.asset(
       ImageAssets.splashScreen,
